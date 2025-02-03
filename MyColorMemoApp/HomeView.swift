6fe9450701160c66c87951ae63caa66abf8e2ad9
daiki.otsuka <tukadai1226@icloud.com>
2025-02-03 23:51:39 +0900

    メモアプリを新規追加

---
+++ MyColorMemoApp/HomeView.swift
@@ -0,0 +1,15 @@
+//
+//  HomeView.swift
+//  MyColorMemoApp
+//
+//  Created by 大塚大樹 on 2025/02/03.
+//
+
+import Foundation
+import UIKit // UIに関するクラスが格納されたモジュール
+
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        print("HomeViewController")
+    }
+}
